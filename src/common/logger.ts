f5371707e65168b82fb88794316a92a1504d4d83
rahuldey98 <r.dey711@gmail.com>
2026-05-21 23:58:29 +0530

    fix timestamp

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -16,9 +16,9 @@ Log levels
 export const logger = winston.createLogger({
         level: process.env.NODE_ENV === "production" ? "info" : "debug",
         format: winston.format.combine(
-            winston.format.json(),
             winston.format.timestamp(),
-            winston.format.errors({stack: true})
+            winston.format.errors({stack: true}),
+            winston.format.json()
         ),
         transports: [
             new winston.transports.Console()
